7a62090e55aac39f649f512256329f54296c3631
HermersonDev <ne.hermerson@gmail.com>
2020-08-12 22:13:54 -0300

    :construction: ensure fetchPoint method return a normalized response

--- src/clients/StormGlass.ts
+++ src/clients/StormGlass.ts
@@ -1,5 +1,35 @@
 import { AxiosStatic } from 'axios'
 
+export type ForecastPoint = {
+  noaa: number;
+}
+
+export type ForecastHour = {
+  readonly time: string;
+  readonly swellDirection: ForecastPoint;
+  readonly swellHeight: ForecastPoint;
+  readonly swellPeriod: ForecastPoint;
+  readonly waveDirection: ForecastPoint;
+  readonly waveHeight: ForecastPoint,
+  readonly windDirection: ForecastPoint;
+  readonly windSpeed: ForecastPoint;
+}
+
+export type ForecastResponse = {
+  hours: ForecastHour[];
+}
+
+export type ForecastHourNormalized = {
+  time: string,
+  swellDirection: number;
+  swellHeight: number;
+  swellPeriod: number;
+  waveDirection: number;
+  waveHeight: number;
+  windDirection: number;
+  windSpeed: number;
+}
+
 export default class StormGlass {
   private readonly BASE_URL = 'https://api.stormglass.io/v2/weather/point?';
   private readonly API_PARAMS =
@@ -7,9 +37,40 @@ export default class StormGlass {
 
   constructor (private clientRequest: AxiosStatic) {}
 
-  public async fetchPoints (lat: number, lng: number): Promise<{}> {
-    return this.clientRequest.get(
+  public async fetchPoints (lat: number, lng: number): Promise<ForecastHourNormalized[]> {
+    const response = await this.clientRequest.get<ForecastResponse>(
       `${this.BASE_URL}params=${this.API_PARAMS}&source=noaa&end=1592113802&lat=${lat}&lng=${lng}`
+    )
+
+    return this.normalizedResponse(response.data)
+  }
+
+  private normalizedResponse (forecastResponse: ForecastResponse): ForecastHourNormalized[] {
+    const filtedHours = forecastResponse.hours.filter(this.isValidHour.bind(this))
+    const hoursNormalized = filtedHours.map(hour => ({
+      time: hour.time,
+      swellDirection: hour.swellDirection.noaa,
+      swellHeight: hour.swellHeight.noaa,
+      swellPeriod: hour.swellPeriod.noaa,
+      waveDirection: hour.waveDirection.noaa,
+      waveHeight: hour.waveHeight.noaa,
+      windDirection: hour.windDirection.noaa,
+      windSpeed: hour.windSpeed.noaa
+    }))
+
+    return hoursNormalized
+  }
+
+  private isValidHour (hour: Partial<ForecastHour>): boolean {
+    return !!(
+      hour.time &&
+      hour.swellDirection?.noaa &&
+      hour.swellHeight?.noaa &&
+      hour.swellPeriod?.noaa &&
+      hour.waveDirection?.noaa &&
+      hour.waveHeight?.noaa &&
+      hour.windDirection?.noaa &&
+      hour.windSpeed?.noaa
     )
   }
 }
