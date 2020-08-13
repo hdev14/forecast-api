d195345a24bb40796f1b7c140cda50e592ae5a92
HermersonDev <ne.hermerson@gmail.com>
2020-08-13 09:27:59 -0300

    :recycle: improve mock to axios and add authorization headers to request

--- src/clients/StormGlass.ts
+++ src/clients/StormGlass.ts
@@ -31,15 +31,20 @@ export type ForecastHourNormalized = {
 }
 
 export default class StormGlass {
-  private readonly BASE_URL = 'https://api.stormglass.io/v2/weather/point?';
+  private readonly BASE_URL = 'https://api.stormglass.io/v2/weather/point?'
   private readonly API_PARAMS =
-    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed'
 
   constructor (private clientRequest: AxiosStatic) {}
 
   public async fetchPoints (lat: number, lng: number): Promise<ForecastHourNormalized[]> {
     const response = await this.clientRequest.get<ForecastResponse>(
-      `${this.BASE_URL}params=${this.API_PARAMS}&source=noaa&end=1592113802&lat=${lat}&lng=${lng}`
+      `${this.BASE_URL}params=${this.API_PARAMS}&source=noaa&end=1592113802&lat=${lat}&lng=${lng}`,
+      {
+        headers: {
+          Authorization: 'fake-token'
+        }
+      }
     )
 
     return this.normalizedResponse(response.data)
--- src/clients/__test__/storm-glass.test.ts
+++ src/clients/__test__/storm-glass.test.ts
@@ -3,16 +3,19 @@ import axios from 'axios'
 import StormGlass from '../StormGlass'
 import stormGlassWeather3HoursFixture from '../../../test/fixtures/stormglass-weather-3-hours.json'
 import stormGlassNormalized3HoursFixture from '../../../test/fixtures/stormglass-normalized-3-hours.json'
+
 jest.mock('axios')
 
 describe('StormGlass Client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>
+
   it('should return the normalized forecast from the StormGlass client', async () => {
     const lat = -33.792726
     const lng = 151.289824
 
-    axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeather3HoursFixture })
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture })
 
-    const stormGlass = new StormGlass(axios)
+    const stormGlass = new StormGlass(mockedAxios)
     const response = await stormGlass.fetchPoints(lat, lng)
     expect(response).toEqual(stormGlassNormalized3HoursFixture)
   })
